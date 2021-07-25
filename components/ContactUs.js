8f5211231921e9ba97dc0966af832d3c0f4bdf61
Eslam <eslam@pop-os.localdomain>
2021-07-25 20:30:13 +0200

    Add the integeration with the backend

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { useTranslation } from "next-i18next";
 
+import axiosInstance from "../libs/axiosInstance";
 export default function ContactUs() {
   const { t } = useTranslation("landing");
 
+  const [phone, setPhone] = useState(null);
+  const [address, setAddress] = useState(null);
+
+  const onChangeAddress = (event) => {
+    setAddress(event.target.value);
+  };
+
+  const onChangePhone = (event) => {
+    setPhone(event.target.value);
+  };
+  const requestFarm = async (event) => {
+    event.preventDefault();
+    const { data } = await axiosInstance({
+      url: `/users`,
+      method: "POST",
+      data: {
+        address: address,
+        phone: phone,
+      },
+    });
+  };
   return (
     <section id="contactus">
       <div className="max-w-6xl mx-auto px-4 sm:px-6">
@@ -29,6 +51,7 @@ export default function ContactUs() {
                   <div className="flex flex-col sm:flex-row justify-center max-w-xs mx-auto sm:max-w-md lg:mx-0">
                     <input
                       type="phone"
+                      onChange={onChangePhone}
                       className="w-full appearance-none bg-white-100 border border-green-400 focus:border-green-500 rounded-sm px-4 py-3 mb-2 sm:mb-0 text-green-600 placeholder-gray-400"
                       placeholder={t("phonenumber")}
                       aria-label="Phone number"
@@ -36,10 +59,15 @@ export default function ContactUs() {
                     <input
                       className="w-full appearance-none bg-white-100 border border-green-400 focus:border-green-500 rounded-sm px-4 py-3 mb-2 sm:mb-0 text-green-600 placeholder-gray-400 lg:mx-2"
                       type="text"
+                      onChange={onChangeAddress}
                       placeholder={t("address")}
                       aria-label="Address"
                     />
-                    <button className="btn text-white bg-green-600 hover:bg-green-700 shadow truncate">
+                    <button
+                      onClick={requestFarm}
+                      disabled={!address || !phone}
+                      className="btn text-white bg-green-600 hover:bg-green-700 shadow truncate disabled:cursor-not-allowed disabled:opacity-50"
+                    >
                       {t("setup_farm_request")}
                     </button>
                   </div>
---
+++ libs/axiosInstance.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+const instance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+export default instance;
