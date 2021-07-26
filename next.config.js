cda5452eef031c81a377015455f30715791e567b
Hesham <heshamhossam@gmail.com>
2021-07-27 00:41:42 +0200

    ignore eslint while building

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ const { i18n } = require('./next-i18next.config');
 module.exports = {
   reactStrictMode: true,
   i18n,
-  ignoreDuringBuilds: true
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  }
 }
