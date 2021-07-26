b6d8b7f077f0695e045ae026125df6cafa7ac808
Hesham <heshamhossam@gmail.com>
2021-07-27 01:16:17 +0200

    try another way for tailwind installtion

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
-import '../styles/globals.css'
+import 'tailwindcss/tailwind.css'
 
 import '../styles/landing/landing.scss'
 import "react-image-gallery/styles/scss/image-gallery.scss";
 import { appWithTranslation } from 'next-i18next';
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'className'
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {
-      opacity: ['disabled'],
-      cursor: ['disabled'],
-    },
+    extend: {},
   },
   plugins: [
     require('tailwindcss-rtl'),
