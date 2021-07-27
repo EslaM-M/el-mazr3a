b05cd695757f890d0896e43642f79078b7c12baf
Hesham <heshamhossam@gmail.com>
2021-07-27 23:43:17 +0200

    remove tailwind config variants disable

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
     extend: {},
   },
   variants: {
-    extend: {
-      opacity: ['disabled'],
-      cursor: ['disabled'],
-    },
+    extend: {},
   },
   plugins: [
     require('tailwindcss-rtl'),
