e2c2f1cf59b343e09c33f1f1442d748a7d3881ca
Hesham <heshamhossam57@gmail.com>
2021-08-09 09:30:33 +0200

    update footer links

--- components/Footer.js
+++ components/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
         <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
           {/* Social links */}
           <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
-            <li>
+            {/* <li>
               <Link href="#" passHref>
                 <span
                   className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
@@ -44,12 +44,13 @@ function Footer() {
                   </svg>
                 </span>
               </Link>
-            </li>
+            </li> */}
             <li className="ml-4">
-              <Link href="#" passHref>
-                <span
+              <Link href="https://www.facebook.com/Mosmera-%D9%85%D8%AB%D9%85%D8%B1%D8%A9-110654507983625" passHref>
+                <a
                   className="flex justify-center items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
                   aria-label="Facebook"
+                  target="_blank"
                 >
                   <svg
                     className="w-8 h-8 fill-current"
@@ -58,7 +59,7 @@ function Footer() {
                   >
                     <path d="M14.023 24L14 17h-3v-3h3v-2c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V14H21l-1 3h-2.72v7h-3.257z" />
                   </svg>
-                </span>
+                </a>
               </Link>
             </li>
           </ul>
